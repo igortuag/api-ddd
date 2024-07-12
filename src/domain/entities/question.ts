d6eb11ef2d374f06d9d19b7fb6a020f7a6b74c92
Tuag <igortuag@gmail.com>
2024-07-12 08:07:11 -0300

    add relation to answer and question

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -4,10 +4,12 @@ export class Question {
   public id: string;
   public title: string;
   public content: string;
+  public authorId: string;
 
-  constructor(title: string, content: string, id?: string) {
+  constructor(title: string, content: string, authorId:string, id?: string) {
     this.title = title;
     this.content = content;
+    this.authorId = authorId;
     this.id = id ?? randomUUID();
   }
 }
