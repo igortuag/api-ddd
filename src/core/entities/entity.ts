7cf4fc7c160f2b7b4a564fffd153ff50b7fa5199
Tuag <igortuag@gmail.com>
2024-07-29 08:34:07 -0300

    add all props setting

--- src/core/entities/entity.ts
+++ src/core/entities/entity.ts
@@ -2,12 +2,14 @@ import { randomUUID } from "crypto"
 
 export class Entity {
   private _id: string
+  protected props: any
 
   get id() {
     return this._id
   }
 
-  constructor(id?: string) {
+  constructor(props: any, id?: string) {
+    this.props = props
     this._id = id ?? randomUUID()
   }
 }
