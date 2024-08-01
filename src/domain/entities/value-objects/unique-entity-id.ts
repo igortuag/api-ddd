8c5209468c10a081b291157a1bc33d9eaffbae5e
Tuag <igortuag@gmail.com>
2024-08-01 08:15:26 -0300

    use random uuid if the id is not pass

--- src/domain/entities/value-objects/unique-entity-id.ts
+++ src/domain/entities/value-objects/unique-entity-id.ts
@@ -1,3 +1,5 @@
+import { randomUUID } from "node:crypto";
+
 export class UniqueEntityID {
   private _value: string;
 
@@ -5,8 +7,8 @@ export class UniqueEntityID {
     return this._value;
   }
 
-  private constructor(id: string) {
-    this._value = id;
+  private constructor(value?: string) {
+    this._value = value ?? randomUUID();
   }
 
   public static create(): UniqueEntityID {
