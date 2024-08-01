02e2a0db4139e760ab5460a27c36f3d7d6b1d551
Tuag <igortuag@gmail.com>
2024-08-01 08:16:18 -0300

    add getter methods

--- src/domain/entities/value-objects/unique-entity-id.ts
+++ src/domain/entities/value-objects/unique-entity-id.ts
@@ -1,17 +1,17 @@
 import { randomUUID } from "node:crypto";
 
 export class UniqueEntityID {
-  private _value: string;
+  private value: string;
 
-  get value(): string {
-    return this._value;
+  toString() {
+    return this.value;
+  }
+
+  toValue() {
+    return this.value;
   }
 
   private constructor(value?: string) {
-    this._value = value ?? randomUUID();
-  }
-
-  public static create(): UniqueEntityID {
-    return new UniqueEntityID("id");
+    this.value = value ?? randomUUID();
   }
 }
