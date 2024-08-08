2a6b428594015345f64a9af1e9662ffb7bed60bb
Tuag <igortuag@gmail.com>
2024-08-08 08:20:26 -0300

    add option creation to answer

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,6 +1,7 @@
 import { randomUUID } from "crypto";
 import { Entity } from "../../core/entities/entity";
 import { UniqueEntityID } from "./value-objects/unique-entity-id";
+import { Optional } from "../../core/types/optional";
 
 interface AnswerProps {
   authorId: UniqueEntityID;
@@ -11,6 +12,21 @@ interface AnswerProps {
 }
 
 export class Answer extends Entity<AnswerProps> {
+  static create(
+    props: Optional<AnswerProps, "createdAt">,
+    id?: UniqueEntityID
+  ) {
+    const question = new Answer(
+      {
+        ...props,
+        createdAt: new Date()
+      },
+      id
+    );
+
+    return question;
+  }
+
   get content() {
     return this.props.content;
   }
