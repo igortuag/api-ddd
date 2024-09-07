64c49d824b2285b2c3cb08545ae1c4f26d3f4e1f
Tuag <igortuag@gmail.com>
2024-09-07 07:17:49 -0300

    add test to get question by slug

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,33 @@
+import { InMemoryQuestionsRepository } from "test/respositories/in-memory-questions-respository";
+import { QuestionsRepository } from "../repositories/question-repository";
+import { CreateQuestionUseCase } from "./create-question";
+import { GetQuestionBySlugUseCase } from "./get-question-by-slug";
+import { Question } from "../../enterprise/entities/question";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
+let sut: GetQuestionBySlugUseCase;
+
+describe("Get Question By Slug", () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository();
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository);
+  });
+
+  it("should be able to get a question by slug", async () => {
+    const newQuestion = Question.create({
+      authorId: new UniqueEntityID(),
+      title: "question title",
+      content: "question content"
+    });
+
+    inMemoryQuestionsRepository.items.push(newQuestion);
+
+    const { question } = await sut.execute({
+      slug: "question-title"
+    }); 
+
+    expect(question.id).toBeTruthy();
+    expect(inMemoryQuestionsRepository.items[0].id).toEqual(question.id);
+  });
+});
