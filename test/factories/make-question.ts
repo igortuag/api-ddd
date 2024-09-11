5d2c0f6caa0d559e47c75cd4041662281d89315e
Tuag <igortuag@gmail.com>
2024-09-11 08:33:27 -0300

    Refactor makeQuestion factory to use faker-js/faker package for generating random title and content

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,3 +1,5 @@
+import { faker } from "@faker-js/faker"
+
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import {
   Question,
@@ -8,8 +10,8 @@ import { Slug } from "@/domain/forum/enterprise/entities/value-objects/slug";
 export function makeQuestion(overrides: Partial<QuestionProps> = {}) {
   const question = Question.create({
     authorId: new UniqueEntityID(),
-    title: "question title",
-    content: "question content",
+    title: faker.lorem.words(3),
+    content: faker.lorem.paragraph(),
     ...overrides
   });
 
