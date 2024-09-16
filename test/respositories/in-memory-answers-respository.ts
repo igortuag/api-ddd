94b84db2c39a064091af030a6bbb3815e843e3bd
Tuag <igortuag@gmail.com>
2024-09-16 09:15:26 -0300

    Refactor InMemoryAnswersRepository to add findById and delete methods

--- test/respositories/in-memory-answers-respository.ts
+++ test/respositories/in-memory-answers-respository.ts
@@ -4,6 +4,14 @@ import { Answer } from "@/domain/forum/enterprise/entities/answer";
 export class InMemoryAnswersRepository implements AnswersRepository {
   private answers: Answer[] = [];
 
+  async findById(id: string): Promise<Answer | null> {
+    return this.answers.find(answer => answer.id.toString() === id) || null;
+  }
+
+  async delete(answer: Answer): Promise<void> {
+    this.answers = this.answers.filter(a => a.id !== answer.id);
+  }
+
   async create(answer: Answer): Promise<Answer> {
     this.answers.push(answer);
 
