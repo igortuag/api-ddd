1bb32405645ef16e4e1110ad0fe8f04cda50e8d1
Tuag <igortuag@gmail.com>
2024-09-19 08:46:28 -0300

    add use case to edit answer

---
+++ src/domain/forum/application/use-cases/edit-answer.ts
@@ -0,0 +1,37 @@
+import { AnswersRepository } from "../repositories/answer-repository";
+
+interface EditAnswerUseCaseRequest {
+  authorId: string;
+  answerId: string;
+  title: string;
+  content: string;
+}
+
+interface EditAnswerUseCaseResponse {}
+
+export class EditAnswerUseCase {
+  constructor(private answerRepository: AnswersRepository) {}
+
+  async execute({
+    answerId,
+    authorId,
+    title,
+    content
+  }: EditAnswerUseCaseRequest): Promise<EditAnswerUseCaseResponse> {
+    const answer = await this.answerRepository.findById(answerId);
+
+    if (!answer) {
+      throw new Error("Answer not found");
+    }
+
+    if (answer.authorId.toString() !== authorId) {
+      throw new Error("Unauthorized");
+    }
+
+    answer.content = content;
+
+    await this.answerRepository.save(answer);
+
+    return {};
+  }
+}
