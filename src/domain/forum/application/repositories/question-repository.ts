a65e991b38e4bb433f84ffc599786601d59e5c8b
Tuag <igortuag@gmail.com>
2024-09-24 07:58:34 -0300

    add pagination params

--- src/domain/forum/application/repositories/question-repository.ts
+++ src/domain/forum/application/repositories/question-repository.ts
@@ -1,9 +1,10 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { Question } from "../../enterprise/entities/question";
 
 export interface QuestionsRepository {
   findById(id: string): Promise<Question | null>;
   findBySlug(slug: string): Promise<Question | null>;
-  findManyRecent(page: number): Promise<Question[]>;
+  findManyRecent(params: PaginationParams): Promise<Question[]>;
   save(question: Question): Promise<Question>;
   create(question: Question): Promise<Question>;
   delete(question: Question): Promise<void>;
