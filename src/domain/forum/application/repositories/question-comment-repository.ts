0d4a751e8dc19571f93e7a95b1bc54c199a49658
Tuag <igortuag@gmail.com>
2024-10-12 07:18:27 -0300

    Refactor QuestionCommentsRepository interface

--- src/domain/forum/application/repositories/question-comment-repository.ts
+++ src/domain/forum/application/repositories/question-comment-repository.ts
@@ -1,5 +1,7 @@
 import { QuestionComment } from "../../enterprise/entities/question-comment";
 
 export interface QuestionCommentsRepository {
+  findById(questionComment: QuestionComment): Promise<void>;
   create(questionComment: QuestionComment): Promise<QuestionComment>;
+  delete(questionComment: QuestionComment): Promise<void>;
 }
