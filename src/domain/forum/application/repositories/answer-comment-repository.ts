31b823319b7cad79fa5fb5b9e038c95d75c082f6
Tuag <igortuag@gmail.com>
2024-10-16 08:07:26 -0300

    Refactor AnswerCommentsRepository interface and InMemoryAnswerCommentsRepository implementation

--- src/domain/forum/application/repositories/answer-comment-repository.ts
+++ src/domain/forum/application/repositories/answer-comment-repository.ts
@@ -1,5 +1,7 @@
 import { AnswerComment } from "../../enterprise/entities/answer-comment";
 
 export interface AnswerCommentsRepository {
+  findById(answerComment: string): Promise<AnswerComment>;
   create(answerComment: AnswerComment): Promise<AnswerComment>;
+  delete(answerComment: AnswerComment): Promise<void>;
 }
--- test/respositories/in-memory-answer-comments-repository.ts
+++ test/respositories/in-memory-answer-comments-repository.ts
@@ -1,12 +1,38 @@
 import { AnswerCommentsRepository } from "@/domain/forum/application/repositories/answer-comment-repository";
 import { AnswerComment } from "@/domain/forum/enterprise/entities/answer-comment";
 
-export class InMemoryAnswerCommentsRepository implements AnswerCommentsRepository {
+export class InMemoryAnswerCommentsRepository
+  implements AnswerCommentsRepository
+{
   public items: AnswerComment[] = [];
+
+  async findById(answerCommentId: string): Promise<AnswerComment> {
+    const foundAnswerComment = this.items.find(
+      (item) => item.id.toString() === answerCommentId
+    );
+
+    if (!foundAnswerComment) {
+      throw new Error("Answer comment not found");
+    }
+
+    return foundAnswerComment;
+  }
 
   async create(answerComment: AnswerComment): Promise<AnswerComment> {
     this.items.push(answerComment);
 
     return answerComment;
+  }
+
+  async delete(answerComment: AnswerComment): Promise<void> {
+    const foundAnswerCommentIndex = this.items.findIndex(
+      (item) => item.id === answerComment.id
+    );
+
+    if (foundAnswerCommentIndex < 0) {
+      throw new Error("Answer comment not found");
+    }
+
+    this.items.splice(foundAnswerCommentIndex, 1);
   }
 }
