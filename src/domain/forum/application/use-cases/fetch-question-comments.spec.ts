1d3a8d789a6d1166dc1f63d8061176b2a14617ab
Tuag <igortuag@gmail.com>
2024-10-23 08:02:38 -0300

    Refactor import statements in fetch-question-comments.spec.ts

--- src/domain/forum/application/use-cases/fetch-question-comments.spec.ts
+++ src/domain/forum/application/use-cases/fetch-question-comments.spec.ts
@@ -1,6 +1,5 @@
 import { FetchQuestionCommentsUseCase } from "./fetch-question-comments";
 
-import { makeAnswer } from "test/factories/make-answer";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { InMemoryQuestionCommentsRepository } from "test/respositories/in-memory-question-comments-repository";
 import { makeQuestionComment } from "test/factories/make-question-comment";
@@ -51,6 +50,7 @@ describe("Fetch Question Comments", () => {
         questionId: new UniqueEntityID("question-1")
       })
     );
+    
 
     const { questionComments } = await sut.execute({
       page: 1,
@@ -58,5 +58,22 @@ describe("Fetch Question Comments", () => {
     });
 
     expect(questionComments).toHaveLength(7);
+  });
+
+  it("should be able to fetch question comments with pagination", async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryQuestionCommentsRepository.create(
+        makeQuestionComment({
+          questionId: new UniqueEntityID("question-1")
+        })
+      );
+    }
+
+    const { questionComments } = await sut.execute({
+      page: 2,
+      questionId: "question-1"
+    });
+
+    expect(questionComments).toHaveLength(2);
   });
 });
