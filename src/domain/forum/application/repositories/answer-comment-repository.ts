fe51b8ed78f63f13eb627d1185d343034818765b
Tuag <igortuag@gmail.com>
2024-10-24 08:05:12 -0300

    Refactor AnswerCommentsRepository to add pagination support

--- src/domain/forum/application/repositories/answer-comment-repository.ts
+++ src/domain/forum/application/repositories/answer-comment-repository.ts
@@ -1,7 +1,12 @@
+import { PaginationParams } from "@/core/repositories/pagination-params";
 import { AnswerComment } from "../../enterprise/entities/answer-comment";
 
 export interface AnswerCommentsRepository {
   findById(answerComment: string): Promise<AnswerComment>;
+  findManyByQuestionId(
+    questionId: string,
+    params: PaginationParams
+  ): Promise<AnswerComment[]>;
   create(answerComment: AnswerComment): Promise<AnswerComment>;
   delete(answerComment: AnswerComment): Promise<void>;
 }
