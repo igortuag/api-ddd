5326d287731ac2da596ef812a94a73a304707f3c
Tuag <igortuag@gmail.com>
2024-10-27 07:03:31 -0300

    Add Right class to handle right values in Either type

--- src/core/either.ts
+++ src/core/either.ts
@@ -2,3 +2,8 @@ export class Left<A> {
   readonly _tag = "Left";
   constructor(readonly value: A) {}
 }
+
+export class Right<B> {
+  readonly _tag = "Right";
+  constructor(readonly value: B) {}
+}
