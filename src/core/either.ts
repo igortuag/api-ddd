5e627aa8c386e5a085f720809b2e229c0d1adf34
Tuag <igortuag@gmail.com>
2024-10-28 07:52:34 -0300

    Refactor Either class to separate Left and Right types

--- src/core/either.ts
+++ src/core/either.ts
@@ -1,8 +1,10 @@
+// Error
 export class Left<A> {
   readonly _tag = "Left";
   constructor(readonly value: A) {}
 }
 
+// Success
 export class Right<B> {
   readonly _tag = "Right";
   constructor(readonly value: B) {}
