c6bffe37e0dc000cd774e39457f428e042fd7986
Tuag <igortuag@gmail.com>
2024-10-31 07:44:07 -0300

    Refactor Either class to use generic types for Left and Right

--- src/core/either.ts
+++ src/core/either.ts
@@ -1,25 +1,28 @@
 // Error
-export class Left<A> {
-  readonly value: any;
+export class Left<L> {
+  readonly value: L;
 
-  constructor(value: any) {
+  constructor(value: L) {
     this.value = value;
   }
 }
 
 // Success
-export class Right {
-  readonly value: any;
+export class Right<R> {
+  readonly value: R;
 
-  constructor(value: any) {
+  constructor(value: R) {
     this.value = value;
   }
 }
 
-export const left = (value: any) => {
+export type Either<L, R> = Left<L> | Right<R>;
+
+
+export const left = <L, R>(value: L) => {
   return new Left(value)
 };
 
-export const right = (value: any) => {
+export const right = <L, R>(value: R) => {
   return new Right(value)
 };
