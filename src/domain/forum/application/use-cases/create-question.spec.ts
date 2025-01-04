4dabfaf54fc5fa0bf411f2247fe18d19f2157aa9
Tuag <igortuag@gmail.com>
2025-01-04 07:19:39 -0300

    Add support for attachments in CreateQuestion use case test

--- src/domain/forum/application/use-cases/create-question.spec.ts
+++ src/domain/forum/application/use-cases/create-question.spec.ts
@@ -11,12 +11,14 @@ describe("CreateQuestionUseCase", () => {
   });
 
   it("should be able to create an question", async () => {
-   const result = await sut.execute({
+    const result = await sut.execute({
       authorId: "author-id",
       title: "question title",
-      content: "question content"
-    }); 
+      content: "question content",
+      attachmentsIds: ["1", "2"]
+    });
 
     expect(result.isRight).toBeTruthy();
+    expect(inMemoryQuestionsRepository.items[0]).toEqual(result.value?.question);
   });
 });
