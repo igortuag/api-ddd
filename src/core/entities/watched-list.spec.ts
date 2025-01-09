30d14a2fa6308c080bc851e6c7c6e06d673b6827
Tuag <igortuag@gmail.com>
2025-01-09 08:46:25 -0300

    Add test for adding new items in NumberWatchedList

--- src/core/entities/watched-list.spec.ts
+++ src/core/entities/watched-list.spec.ts
@@ -11,4 +11,13 @@ describe("WatchedList", () => {
     const list = new NumberWatchedList([1, 2, 3]);
     expect(list.currentItems).toHaveLength(3);
   });
+
+  it("should be able to add new items to the list", () => {
+    const list = new NumberWatchedList([1, 2, 3]);
+    
+    list.add(4);
+
+    expect(list.currentItems).toHaveLength(4);
+    expect(list.getNewItems()).toEqual(4);
+  })
 });
