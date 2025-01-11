a201e73925bf4c36d4bbabd04627003723ffbbd7
Tuag <igortuag@gmail.com>
2025-01-11 07:06:17 -0300

    Add tests for removing items and re-adding in NumberWatchedList

--- src/core/entities/watched-list.spec.ts
+++ src/core/entities/watched-list.spec.ts
@@ -14,19 +14,30 @@ describe("WatchedList", () => {
 
   it("should be able to add new items to the list", () => {
     const list = new NumberWatchedList([1, 2, 3]);
-    
+
     list.add(4);
 
     expect(list.currentItems).toHaveLength(4);
     expect(list.getNewItems()).toEqual([4]);
-  })
+  });
 
-    it("should be able to remove items from the list", () => {
-      const list = new NumberWatchedList([1, 2, 3]);
+  it("should be able to remove items from the list", () => {
+    const list = new NumberWatchedList([1, 2, 3]);
 
-      list.remove(2);
+    list.remove(2);
 
-      expect(list.currentItems).toHaveLength(2);
-      expect(list.getRemovedItems()).toEqual([2]);
-    });
+    expect(list.currentItems).toHaveLength(2);
+    expect(list.getRemovedItems()).toEqual([2]);
+  });
+
+  it("should be able to add an item even if it was removed before", () => {
+    const list = new NumberWatchedList([1, 2, 3]);
+
+    list.remove(2);
+    list.add(2);
+
+    expect(list.currentItems).toHaveLength(3);
+    expect(list.getRemovedItems()).toEqual([]);
+    expect(list.getNewItems()).toEqual([]);
+  });
 });
