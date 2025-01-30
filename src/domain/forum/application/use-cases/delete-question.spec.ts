dcdade0dd982a9649aaca6e1da00eebd0a54d62e
Igor Tuag <igortuag@gmail.com>
2025-01-30 08:19:54 -0300

    Add test for deleting a question with attachments in DeleteQuestionUseCase

--- src/domain/forum/application/use-cases/delete-question.spec.ts
+++ src/domain/forum/application/use-cases/delete-question.spec.ts
@@ -4,6 +4,7 @@ import { makeQuestion } from "test/factories/make-question";
 import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { NotAllowedError } from "./errors/not-allowed-error";
 import { InMemoryQuestionAttachmentsRepository } from "test/respositories/in-memory-question-attachments-repository";
+import { makeQuestionAttachment } from "test/factories/make-question-attachment";
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository;
 let inMemoryQuestionAttachmentsRepository: InMemoryQuestionAttachmentsRepository;
@@ -34,6 +35,36 @@ describe("DeleteQuestionUseCase", () => {
     });
 
     expect(inMemoryQuestionsRepository.items).toHaveLength(0);
+  });
+
+  it("should be able to delete a question", async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID("author-1")
+      },
+      new UniqueEntityID("question-1")
+    );
+
+    await inMemoryQuestionsRepository.create(newQuestion);
+
+    inMemoryQuestionAttachmentsRepository.items.push(
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityID("1")
+      }),
+      makeQuestionAttachment({
+        questionId: newQuestion.id,
+        attachmentId: new UniqueEntityID("2")
+      })
+    );
+
+    await sut.execute({
+      questionId: "question-1",
+      authorId: "author-1"
+    });
+
+    expect(inMemoryQuestionsRepository.items).toHaveLength(0);
+    expect(inMemoryQuestionAttachmentsRepository.items).toHaveLength(0);
   });
 
   it("should not be able to delete a question if the author is different", async () => {
