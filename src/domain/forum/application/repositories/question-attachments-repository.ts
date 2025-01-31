a65fb067829f7d40779b051d521a2f47d921cf3c
Igor Tuag <igortuag@gmail.com>
2025-01-31 08:14:37 -0300

    Add deleteManyByQuestionId method to QuestionAttachmentsRepository and update InMemoryQuestionsRepository constructor

--- src/domain/forum/application/repositories/question-attachments-repository.ts
+++ src/domain/forum/application/repositories/question-attachments-repository.ts
@@ -2,4 +2,5 @@ import { QuestionAttachment } from "../../enterprise/entities/answer-attatchment
 
 export interface QuestionAttachmentsRepository {
   findManyByQuestionId(questionId: string): Promise<QuestionAttachment[]>;
+  deleteManyByQuestionId(questionId: string): Promise<void>;
 }
