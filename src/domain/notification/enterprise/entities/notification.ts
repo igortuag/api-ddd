54cb785b416a95d036e230a017385826290481c2
Igor Tuag <igortuag@gmail.com>
2025-03-07 08:46:42 -0300

    Enhance Notification entity with recipientId property and getter methods

--- src/domain/notification/enterprise/entities/notification.ts
+++ src/domain/notification/enterprise/entities/notification.ts
@@ -1,10 +1,28 @@
 import { Entity } from "@/core/entities/entity";
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 
 interface NotificationProps {
+  recipientId: UniqueEntityID;
   title: string;
   content: string;
   createdAt: Date;
   readAt?: Date;
 }
 
-export class Notification extends Entity<NotificationProps> {}
+export class Notification extends Entity<NotificationProps> {
+  get recipientId() {
+    return this.props.recipientId;
+  }
+  get title() {
+    return this.props.title;
+  }
+  get content() {
+    return this.props.content;
+  }
+  get createdAt() {
+    return this.props.createdAt;
+  }
+  get readAt() {
+    return this.props.readAt;
+  }
+}
