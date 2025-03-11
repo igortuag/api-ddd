e419f534a3fdeb026c9ec66fb3687073e07ef94b
Igor Tuag <igortuag@gmail.com>
2025-03-11 08:25:31 -0300

    Refactor NotificationsRepository to return created Notification and add in-memory implementation for testing

--- src/domain/notification/application/repositories/notification-repository.ts
+++ src/domain/notification/application/repositories/notification-repository.ts
@@ -1,5 +1,5 @@
 import { Notification } from "../../enterprise/entities/notification";
 
 export interface NotificationsRepository {
-  create(notification: Notification): Promise<void>;
+  create(notification: Notification): Promise<Notification>;
 }
