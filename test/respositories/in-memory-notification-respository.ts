171d96268700cb723a1609150ac446f8fada5a95
Igor Tuag <igortuag@gmail.com>
2025-03-15 06:02:47 -0300

    Add findById and save methods to InMemoryNotificationsRepository

--- test/respositories/in-memory-notification-respository.ts
+++ test/respositories/in-memory-notification-respository.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "@/core/entities/unique-entity-id";
 import { NotificationsRepository } from "@/domain/notification/application/repositories/notification-repository";
 import { Notification } from "@/domain/notification/enterprise/entities/notification";
 
@@ -8,6 +9,22 @@ export class InMemoryNotificationsRepository
 
   async create(notification: Notification): Promise<Notification> {
     this.items.push(notification);
+
+    return notification;
+  }
+
+  async findById(id: string): Promise<Notification | null> {
+    const notification = this.items.find(
+      (item) => item.id === new UniqueEntityID(id)
+    );
+
+    return notification || null;
+  }
+
+  async save(notification: Notification): Promise<Notification> {
+    const index = this.items.findIndex((item) => item.id === notification.id);
+
+    this.items[index] = notification;
 
     return notification;
   }
