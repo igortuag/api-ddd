8f0f99b1ea1d61db3fecf805eb4dcc12551df70c
Igor Tuag <igortuag@gmail.com>
2025-03-22 09:23:10 -0300

    Add equals method to UniqueEntityID for value comparison and improve formatting

--- src/core/entities/unique-entity-id.ts
+++ src/core/entities/unique-entity-id.ts
@@ -1,17 +1,21 @@
-import { randomUUID } from 'node:crypto'
+import { randomUUID } from "node:crypto";
 
 export class UniqueEntityID {
-  private value: string
+  private value: string;
 
   toString() {
-    return this.value
+    return this.value;
   }
 
   toValue() {
-    return this.value
+    return this.value;
   }
 
   public constructor(value?: string) {
-    this.value = value ?? randomUUID()
+    this.value = value ?? randomUUID();
+  }
+
+  equals(id: UniqueEntityID): boolean {
+    return this.value === id.value;
   }
 }
