dd35ad01b73d51d8e284085846dcc03a8d9a2d18
Igor Tuag <igortuag@gmail.com>
2025-04-13 06:21:55 -0300

    Refactor InMemoryQuestionsRepository for consistency and readability

--- test/respositories/in-memory-questions-respository.ts
+++ test/respositories/in-memory-questions-respository.ts
@@ -1,51 +1,56 @@
-import { QuestionAttachmentsRepository } from "@/domain/forum/application/repositories/question-attachments-repository";
-import { QuestionsRepository } from "@/domain/forum/application/repositories/question-repository";
-import { Question } from "@/domain/forum/enterprise/entities/question";
+import { DomainEvents } from '@/core/events/domain-events'
+import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
+import { QuestionsRepository } from '@/domain/forum/application/repositories/question-repository'
+import { Question } from '@/domain/forum/enterprise/entities/question'
 
 export class InMemoryQuestionsRepository implements QuestionsRepository {
-  public items: Question[] = [];
+  public items: Question[] = []
 
   constructor(
-    readonly questionAttachmentsRepository: QuestionAttachmentsRepository
+    readonly questionAttachmentsRepository: QuestionAttachmentsRepository,
   ) {}
 
   async findById(id: string): Promise<Question | null> {
-    return this.items.find((question) => question.id.toString() === id) ?? null;
+    return this.items.find((question) => question.id.toString() === id) ?? null
   }
 
   async findBySlug(slug: string): Promise<Question | null> {
-    return this.items.find((question) => question.slug.value === slug) ?? null;
+    return this.items.find((question) => question.slug.value === slug) ?? null
   }
 
   async findManyRecent({ page }: { page: number }): Promise<Question[]> {
     const questions = this.items
       .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
-      .slice((page - 1) * 20, page * 20);
+      .slice((page - 1) * 20, page * 20)
 
-    return questions;
+    return questions
   }
 
-  async create(question: any): Promise<Question> {
-    this.items.push(question);
+  async create(question: Question): Promise<Question> {
+    this.items.push(question)
 
-    return question;
+    DomainEvents.dispatchEventsForAggregate(question.id)
+
+    return question
   }
 
   async save(question: Question): Promise<Question> {
-    const itemIndex = this.items.findIndex((item) => item.id === question.id);
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
 
-    this.items[itemIndex] = question;
+    this.items[itemIndex] = question
 
-    return question;
+    DomainEvents.dispatchEventsForAggregate(question.id)
+
+    return question
   }
 
   async delete(question: Question): Promise<void> {
-    const itemIndex = this.items.findIndex((item) => item.id === question.id);
+    const itemIndex = this.items.findIndex((item) => item.id === question.id)
 
-    this.items.splice(itemIndex, 1);
+    this.items.splice(itemIndex, 1)
 
     this.questionAttachmentsRepository.deleteManyByQuestionId(
-      question.id.toString()
-    );
+      question.id.toString(),
+    )
   }
 }
