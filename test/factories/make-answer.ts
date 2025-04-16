d0eb79e3f383d0923389b9c6e89966f3d4ab29d8
Igor Tuag <igortuag@gmail.com>
2025-04-16 09:23:54 -0300

    Refactor test cases for FetchQuestionAnswersUseCase and improve makeAnswer function formatting

--- test/factories/make-answer.ts
+++ test/factories/make-answer.ts
@@ -1,23 +1,21 @@
-import { faker } from "@faker-js/faker"
+import { faker } from '@faker-js/faker'
 
-import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import {
-  Answer,
-  AnswerProps
-} from "@/domain/forum/enterprise/entities/answer";
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Answer, AnswerProps } from '@/domain/forum/enterprise/entities/answer'
 
-export function makeAnswer(overrides: Partial<AnswerProps> = {},
-  id?: UniqueEntityID
+export function makeAnswer(
+  overrides: Partial<AnswerProps> = {},
+  id?: UniqueEntityID,
 ) {
   const answer = Answer.create(
     {
       authorId: new UniqueEntityID(),
       questionId: new UniqueEntityID(),
       content: faker.lorem.paragraph(),
-      ...overrides
+      ...overrides,
     },
-    id
-  );
+    id,
+  )
 
-  return answer;
+  return answer
 }
