20b5e4193390c10663f55a85cb91b41816cb0266
Igor Tuag <igortuag@gmail.com>
2025-04-19 07:00:35 -0300

    Refactor answer-question.spec.ts and in-memory-answers-respository.ts for consistency and readability

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,40 +1,40 @@
-import { AnswerQuestionUseCase } from "./answer-question";
-import { AnswersRepository } from "../repositories/answer-repository";
-import { InMemoryAnswersRepository } from "test/respositories/in-memory-answers-respository";
-import { UniqueEntityID } from "@/core/entities/unique-entity-id";
-import { InMemoryAnswerAttachmentsRepository } from "test/respositories/in-memory-answer-attachments-repository";
+import { AnswerQuestionUseCase } from './answer-question'
+import { InMemoryAnswersRepository } from 'test/respositories/in-memory-answers-respository'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { InMemoryAnswerAttachmentsRepository } from 'test/respositories/in-memory-answer-attachments-repository'
 
-let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository;
-let inMemoryAnswerQuestionsRepository: InMemoryAnswersRepository;
-let sut: AnswerQuestionUseCase;
+let inMemoryAnswerAttachmentsRepository: InMemoryAnswerAttachmentsRepository
+let inMemoryAnswerQuestionsRepository: InMemoryAnswersRepository
+let sut: AnswerQuestionUseCase
 
-describe("Create Question", () => {
+describe('Create Question', () => {
   beforeEach(() => {
-    inMemoryAnswerAttachmentsRepository = new InMemoryAnswerAttachmentsRepository();
+    inMemoryAnswerAttachmentsRepository =
+      new InMemoryAnswerAttachmentsRepository()
     inMemoryAnswerQuestionsRepository = new InMemoryAnswersRepository(
-      inMemoryAnswerAttachmentsRepository
-    );
-    sut = new AnswerQuestionUseCase(inMemoryAnswerQuestionsRepository);
-  });
+      inMemoryAnswerAttachmentsRepository,
+    )
+    sut = new AnswerQuestionUseCase(inMemoryAnswerQuestionsRepository)
+  })
 
-  it("should be able create an answer", async () => {
+  it('should be able create an answer', async () => {
     const result = await sut.execute({
-      instructorId: "instructor-id",
-      questionId: "question-id",
-      content: "answer content",
-      attachmentsIds: ["1", "2"]
-    });
+      instructorId: 'instructor-id',
+      questionId: 'question-id',
+      content: 'answer content',
+      attachmentsIds: ['1', '2'],
+    })
 
-    expect(result.isRight).toBeTruthy();
+    expect(result.isRight).toBeTruthy()
 
     expect(
-      inMemoryAnswerQuestionsRepository.items[0].attachmentsIds.currentItems
-    ).toHaveLength(2);
+      inMemoryAnswerQuestionsRepository.items[0].attachments.currentItems,
+    ).toHaveLength(2)
     expect(
-      inMemoryAnswerQuestionsRepository.items[0].attachmentsIds.currentItems
+      inMemoryAnswerQuestionsRepository.items[0].attachments.currentItems,
     ).toEqual([
-      expect.objectContaining({ attachmentId: new UniqueEntityID("1") }),
-      expect.objectContaining({ attachmentId: new UniqueEntityID("2") })
-    ]);
-  });
-});
+      expect.objectContaining({ attachmentId: new UniqueEntityID('1') }),
+      expect.objectContaining({ attachmentId: new UniqueEntityID('2') }),
+    ])
+  })
+})
