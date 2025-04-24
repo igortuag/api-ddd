3575e2904afd210c29ffa2ac7ba7b191dd4b1faf
Igor Tuag <igortuag@gmail.com>
2025-04-24 08:39:30 -0300

    Refactor DeleteQuestionUseCaseResponse type for clarity

--- src/domain/forum/application/use-cases/delete-question.ts
+++ src/domain/forum/application/use-cases/delete-question.ts
@@ -10,7 +10,7 @@ interface DeleteQuestionUseCaseRequest {
 
 type DeleteQuestionUseCaseResponse = Either<
   ResourceNotFoundError | NotAllowedError,
-  {}
+  Record<string, never>
 >
 
 export class DeleteQuestionUseCase {
