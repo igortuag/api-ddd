631c7143cb843ed8b0fd5dc1f4c44f1a1e982397
Igor Tuag <igortuag@gmail.com>
2025-04-26 07:10:41 -0300

    Refactor response type in DeleteAnswerCommentUseCase and DeleteAnswerUseCase for clarity

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -10,7 +10,7 @@ interface DeleteAnswerCommentUseCaseRequest {
 
 type DeleteAnswerCommentUseCaseResponse = Either<
   ResourceNotFoundError | NotAllowedError,
-  {}
+  Record<string, never>
 >
 
 export class DeleteAnswerCommentUseCase {
